15152f321dac73c8e94a4070d43a9f8d3542dc6b
Kevin <kevin.troyan@gmail.com>
2023-11-21 16:33:43 -0600

    dark mode

--- components/contact.tsx
+++ components/contact.tsx
@@ -6,6 +6,7 @@ import { motion } from 'framer-motion'
 import { useSectionInView } from '@/lib/hooks';
 import { sendEmail } from '@/actions/sendEmail';
 import SubmitBtn from './submit-btn';
+import toast from 'react-hot-toast';
 
 export default function Contact() {
 
@@ -36,11 +37,11 @@ export default function Contact() {
             const { data, error } = await sendEmail(formData);
 
             if (error) {
-                alert(error);
+                toast.error(error);
                 return;
             }
 
-            alert('Email sent successfully!')
+            toast.success('Email sent successfully!')
         }}
         
         >
